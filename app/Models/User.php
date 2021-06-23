361889b092622a6e3478691a4feaf6cec83d014c
shoxrux1996 <shohrux.shomaxmudov@gmail.com>
2021-06-23 18:08:21 +0500

    Models and migrations

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+}
